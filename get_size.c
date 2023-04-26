b3ead2b1932f214bb89f76f418680132987d2f4f
AhmedShahine1 <102306170+AhmedShahine1@users.noreply.github.com>
2023-04-26 03:12:14 +0200

    Update get_size.c

--- get_size.c
+++ get_size.c
@@ -2,25 +2,25 @@
 
 /**
  * get_size - Calculates the size to cast the argument
- * @f: Formatted string in which to print the arguments
- * @t: List of arguments to be printed.
+ * @format: Formatted string in which to print the arguments
+ * @i: List of arguments to be printed.
  *
  * Return: Precision.
  */
-int get_size(const char *f, int *t)
+int get_size(const char *format, int *i)
 {
-	int curr_i = *t + 1;
+	int curr_i = *i + 1;
 	int size = 0;
 
-	if (f[curr_i] == 'l')
+	if (format[curr_i] == 'l')
 		size = S_LONG;
-	else if (f[curr_i] == 'h')
+	else if (format[curr_i] == 'h')
 		size = S_SHORT;
 
 	if (size == 0)
-		*t = curr_i - 1;
+		*i = curr_i - 1;
 	else
-		*t = curr_i;
+		*i = curr_i;
 
 	return (size);
 }
