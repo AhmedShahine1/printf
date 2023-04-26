f9070ec2460e2314e876ed2d2c3e642233bafb59
AhmedShahine1 <102306170+AhmedShahine1@users.noreply.github.com>
2023-04-26 03:04:35 +0200

    Update handle_print.c

--- handle_print.c
+++ handle_print.c
@@ -4,15 +4,15 @@
  * @fmt: Formatted string in which to print the arguments.
  * @list: List of arguments to be printed.
  * @ind: ind.
- * @buff: Buffer array to handle print.
- * @f: Calculates active flags
- * @w: get width.
- * @p: Precision specification
- * @s: Size specifier
+ * @buffer: Buffer array to handle print.
+ * @flags: Calculates active flags
+ * @width: get width.
+ * @precision: Precision specification
+ * @size: Size specifier
  * Return: 1 or 2;
  */
-int handle_print(const char *fmt, int *ind, va_list list, char buff[],
-	int f, int w, int p, int s)
+int handle_print(const char *fmt, int *ind, va_list list, char buffer[],
+	int flags, int width, int precision, int size)
 {
 	int i, unknow_len = 0, printed_chars = -1;
 	fmt_t fmt_types[] = {
@@ -24,7 +24,7 @@ int handle_print(const char *fmt, int *ind, va_list list, char buff[],
 	};
 	for (i = 0; fmt_types[i].fmt != '\0'; i++)
 		if (fmt[*ind] == fmt_types[i].fmt)
-			return (fmt_types[i].fn(list, buff, f, w, p, s));
+			return (fmt_types[i].fn(list, buffer, flags, width, precision, size));
 
 	if (fmt_types[i].fmt == '\0')
 	{
